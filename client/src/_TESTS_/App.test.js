49430d85d2be6e9992b0b6ad357d6ee3a976a4a5
deejayeaster <deejayeaster@live.com>
2019-08-09 14:45:58 -0400

    third

--- client/src/_TESTS_/App.test.js
+++ client/src/_TESTS_/App.test.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Button } from "semantic-ui-react";
 import { act, fireEvent } from "react-dom/test-utils";
 
 it('calls "onClick" prop on button click', () => {
   // Render new instance in every test to prevent leaking state
   const onClick = jest.fn();
-  const { getByText } = render(<Button onClick={onClick} />);
+  const { getByText } = <Button onClick={onClick} />;
 
   fireEvent.click(getByText(/click me nao/i));
   expect(onClick).toHaveBeenCalled();
